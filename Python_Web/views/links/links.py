03c995d66f105a045752701116f6418b411a598c
Gerald Lopez <managementduscht22@gmail.com>
2023-12-19 01:07:40 +0100

    Add 4 link_button

--- Python_Web/views/links/links.py
+++ Python_Web/views/links/links.py
@@ -1,7 +1,11 @@
 import reflex as rx
-
+from Python_Web.components.link_button import Link_Button, link_button
 
 def links() -> rx.Component:
     return rx.vstack(
-        rx.button("Click Me!")
+        link_button("Youtube"),
+        link_button("Instragram"),
+        link_button("X"),
+        link_button("Facebook")
+        
     )
